4cc91c4a7fc4c3d34ec763497f53035246415610
k4itrun <103044629+k4itrun@users.noreply.github.com>
2025-04-19 16:00:53 -0500

    Update prettier.config.js

--- prettier.config.js
+++ prettier.config.js
@@ -1,6 +1,6 @@
 // @ts-check
 /** @type {import('prettier').Options} */
-import prettierConfig from '@k4i/prettier-config';
+import prettierConfig from '@k4i/config/prettier-config';
 
 export default {
   ...prettierConfig,
