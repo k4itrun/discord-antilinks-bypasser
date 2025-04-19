1c3e52971302e08724cbbf486b655bbbe2448a05
k4itrun <103044629+k4itrun@users.noreply.github.com>
2025-04-19 16:50:08 -0500

    refactor: code

--- eslint.config.js
+++ eslint.config.js
@@ -1,7 +1,7 @@
 // @ts-check
-/** @type {import('eslint').Linter.Config} */
 import eslintConfig from '@k4i/config/eslint-config';
 
+/** @type {import('eslint').Linter.Config[]} */
 export default [
   // prettier
   ...eslintConfig.base,
