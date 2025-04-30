9c856302558ac1c89fd4369b55e8df6b5c574cc1
k4itrun <103044629+k4itrun@users.noreply.github.com>
2025-04-30 15:28:39 -0500

    refactor: recode

--- eslint.config.js
+++ eslint.config.js
@@ -1,17 +1,20 @@
 // @ts-check
-import eslintConfig from '@k4i/config/eslint-config';
+import eslintConfig from "@k4i/config/eslint-config";
+import { defineConfig } from "eslint/config";
 
 /** @type {import('eslint').Linter.Config[]} */
-export default [
-  ...eslintConfig.base,
-  ...eslintConfig.node,
-  ...eslintConfig.typescript,
-  ...eslintConfig.prettier,
+export default defineConfig([
+ eslintConfig.base,
+ eslintConfig.node,
+ eslintConfig.typescript,
+ eslintConfig.prettier,
+ [
   {
-    name: 'Override',
-    rules: {
-      'require-await': 'off',
-      'node/no-process-exit': 'off',
-    },
+   name: "Override",
+   rules: {
+    "require-await": "off",
+    "node/no-process-exit": "off",
+   },
   },
-];
+ ],
+]);
